f3c00e862790e485efb233e9198105750ffbbf3f
lukedavisseo <92871067+lukedavisseo@users.noreply.github.com>
2023-01-24 14:56:28 +0000

    Add files via upload

---
+++ sitemap_check.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import advertools as adv
+import pandas as pd
+
+urls_dict = {
+	'URL': [],
+	'Status': []
+}
+
+st.header('Is it really not in the sitemap?')
+
+st.info("This script takes a CSV export of 'Primary Pages Not in Sitemaps' from Lumar and checks the site's sitemap to see if they really are missing.")
+
+# urls_to_check = st.text_area('Enter the URLs you want to check, one per line')
+
+crawl_csv = st.file_uploader('Upload your Lumar CSV file')
+urls_to_check = st.text_area('Or add a list of URLs to check, 1 per line')
+sitemap_link = st.text_input('Enter XML sitemap to check')
+submit = st.button('Submit')
+
+if submit and crawl_csv:
+
+	crawl_csv_url_list = pd.read_csv(crawl_csv)['url'].tolist()
+	sitemap_df = adv.sitemap_to_df(sitemap_link)
+	sitemap_url_list = sitemap_df['loc'].to_list()
+
+	for url in crawl_csv_url_list:
+		urls_dict['URL'].append(url)
+		if url in sitemap_url_list:			
+			urls_dict['Status'].append('Found in sitemap')
+		else:
+			urls_dict['Status'].append('Not found in sitemap')
+
+elif submit and urls_to_check:
+
+	sitemap_df = adv.sitemap_to_df(sitemap_link)
+	sitemap_url_list = sitemap_df['loc'].to_list()
+
+	url_list = [url for url in urls_to_check.split('\n')]
+
+	for u in url_list:
+		urls_dict['URL'].append(u)
+		if u in sitemap_url_list:			
+			urls_dict['Status'].append('Found in sitemap')
+		else:
+			urls_dict['Status'].append('Not found in sitemap')
+
+
+	df = pd.DataFrame(urls_dict)
+	st.dataframe(df)
+	df_csv = df.to_csv(index=False)
+	st.download_button(label="Download data as CSV", data=df_csv, file_name='urls_in_sitemap.csv', mime='text/csv')
